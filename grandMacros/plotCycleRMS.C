af177a157aae07a14a22298a795f9f222b29797b
Compton Polarimeter (D.Gaskell) <compton@compton.jlab.org>
2020-08-17 19:40:53 -0400

    Committing earlier changes that did not get pushed

--- grandMacros/plotCycleRMS.C
+++ grandMacros/plotCycleRMS.C
@@ -33,31 +33,52 @@ void plotCycleRMS(Int_t prexOrCrex){
     exit(1);
   }
   
-  TFile *f = TFile::Open(Form("%s/%sComptonGrand.root", getenv("COMPMON_GRAND"), exptStr.Data()));
+  TFile *f = TFile::Open(Form("%s/%sGrandCompton.root", getenv("COMPMON_GRAND"), exptStr.Data()));
   TTree *cyc = (TTree *)f->Get("cyc");
   
   Int_t runNum, cycNum;
-  DataVar fOff, lOff;
+  DataVar on, fOff, lOff;
   cyc->SetBranchAddress("runNum", &runNum);
-  cyc->SetBranchAddress("cycleNum", &cycNum);
+  cyc->SetBranchAddress("cycleNum", &cycNum); 
+  cyc->SetBranchAddress("Acc0LasOn", &on);
   cyc->SetBranchAddress("Acc0LasOff1", &fOff);
-  cyc->SetBranchAddress("Acc0LasOff2", %lOff);
+  cyc->SetBranchAddress("Acc0LasOff2", &lOff);
   
-  TGraphErrors *hF = new TH1F("hF", Form("%s Acc0 RMS by Cycle", exptName.Data()),
-                              (Int_t)cyc->GetEntries(), 0, (Int_t)cyc->GetEntries());
-  TGraphErrors *hL = new TH1F("hL", Form("%s Acc0 RMS by Cycle", exptName.Data()),
-                              (Int_t)cyc->GetEntries(), 0, (Int_t)cyc->GetEntries());
+  TH1F *hF = new TH1F("hF", Form("%s Acc0 (Las Off) RMS by Cycle", exptName.Data()),
+                      (Int_t)cyc->GetEntries(), 0, (Int_t)cyc->GetEntries());
+  TH1F *hL = new TH1F("hL", Form("%s Acc0 (Las Off) RMS by Cycle", exptName.Data()),
+                      (Int_t)cyc->GetEntries(), 0, (Int_t)cyc->GetEntries());
+  TH1F *hO = new TH1F("hO", Form("%s Acc0 (Las On) RMS by Cycle", exptName.Data()),
+                      (Int_t)cyc->GetEntries(), 0, (Int_t)cyc->GetEntries());
 
   for(Int_t i = 0; i < cyc->GetEntries(); i++){
     cyc->GetEntry(i);
-    gF->SetPoint(i, i+1, fOff.rms);
-    gF->SetPointError(i, 0, fOff.rmsErr);
-    gL->SetPoint(i, i+1, lOff.rms);
-    gL->SetPointError(i, 0, lOff.rmsErr);
+    hF->SetBinContent(i+1, fOff.rms);
+    hF->SetBinError(i+1, fOff.rmsErr);
+    hL->SetBinContent(i+1, lOff.rms);
+    hL->SetBinError(i+1, lOff.rmsErr);
+    hO->SetBinContent(i+1, on.rms);
+    hO->SetBinError(i+1, on.rmsErr);
+    if(i % 100 == 0){
+      hF->GetXaxis()->SetBinLabel(i+1, Form("%i.%i", runNum, cycNum));
+      hL->GetXaxis()->SetBinLabel(i+1, Form("%i.%i", runNum, cycNum));
+      hO->GetXaxis()->SetBinLabel(i+1, Form("%i.%i", runNum, cycNum));
+    }
   }
 
+  gStyle->SetOptStat(0);
   TCanvas *cRMS = new TCanvas("cRMS", "RMS Graph", 1200, 800);
+  cRMS->SetGridx(); cRMS->SetGridy();
+  TCanvas *cOnRMS = new TCanvas("cOnRMS", "RMS Graph (Las On)", 1200, 800);
+  cOnRMS->SetGridx(); cOnRMS->SetGridy();
   cRMS->cd();
-  hF->GetXaxis()->SetTitle("cycle num");
-  hL->GetXaxis()->SetTitle("cycle num");
+  hF->GetXaxis()->SetTitle("cycle num"); hL->GetXaxis()->SetTitle("cycle num");
+  hF->GetYaxis()->SetTitle("Acc0 RMS"); hL->GetYaxis()->SetTitle("Acc0 RMS");
+  hF->SetMarkerColor(kRed); hL->SetMarkerColor(kRed + 2);
+  hF->SetLineColor(kRed); hL->SetLineColor(kRed + 2);
+  hF->Draw(); hL->Draw("same");
+  cOnRMS->cd();
+  hO->GetXaxis()->SetTitle("cycle num"); hO->GetYaxis()->SetTitle("Acc0 RMS");
+  hO->SetMarkerColor(kGreen + 2); hO->SetLineColor(kGreen + 2);
+  hO->Draw();
 }
